358050587d5fb15006711df1d0be114ec6be229e
Claude Ayitey <clauditey@gmail.com>
2013-10-29 15:05:17 +0000

    added the sign in with Google+ button on the index page

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 
         <div class="container">
             <div class="row signup">
-                <p><a class="btn btn-lg btn-success" href="/learner">Log in with Gmail</a></p>
+                <div class="col-md-2"><a href="/learner"><img src="assets/img/signingoogle.webp" class="google" alt="Sign In With Google"></a></div>
             </div>
         </div>
 
