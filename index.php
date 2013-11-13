235a4a3ab374032adef54fee45f3b64790cfb67a
Claude Ayitey <clauditey@gmail.com>
2013-11-13 01:54:35 +0000

    renamed the files back to php

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="keyowrds" content="online learning, online student program, study chum, studychum, find students, students with same course">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>StudyChum, find people with similar learning interests and study together</title>
+        <link rel="stylesheet" href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600|Oxygen'>
+        <link rel="stylesheet" href='http://fonts.googleapis.com/css?family=Fondamento'>
+        <link rel="stylesheet" href="assets/css/bs.min.css">
+        <link rel="stylesheet" href="assets/css/app.css">
+        <link rel="stylesheet" href="assets/css/index.css">
+        <link rel="shortcut icon" href="assets/img/favicon.ico">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row header">
+                <div class="col-md-9">
+                    <div class="headline">StudyChum</div>
+                    <div class="tagline">
+
+                    </div>
+                </div>
+                <div class="col-md-3">
+                <a class="press orange google" href="/create">Sign in with Google</a>
+                </div>
+            </div>
+        </div>
+
+        <div id="myCarousel" class="carousel slide">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active">
+                    <div class="fill slide1"></div>
+                    <div class="carousel-caption mast">
+                        <h1>Connect with chums and tutors</h1>
+                        <!--div class="input-group input-group-lg">
+                            <input type="text" class="form-control" placeholder="email address">
+                            <span class="input-group-btn">
+                                <button class="btn btn-success" type="button">I'm in.</button>
+                            </span>
+                        </div-->
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="fill slide2"></div>
+                    <div class="carousel-caption mast">
+                        <h1>Share resources across groups</h1>
+                        <!--div class="input-group input-group-lg">
+                            <input type="text" class="form-control" placeholder="email address">
+                            <span class="input-group-btn">
+                                <button class="btn btn-success" type="button">I'm in.</button>
+                            </span>
+                        </div-->
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="fill slide3"></div>
+                    <div class="carousel-caption mast">
+                        <h1>Enjoy learning together</h1>
+                        <!--div class="input-group input-group-lg">
+                            <input type="text" class="form-control" placeholder="email address">
+                            <span class="input-group-btn">
+                                <button class="btn btn-success" type="button">I'm in.</button>
+                            </span>
+                        </div-->
+
+                    </div>
+                </div>
+            </div>
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="icon-prev"></span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="icon-next"></span>
+            </a>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                </div>
+            </div>
+            <div class="row features">
+                <div class="col-md-3">
+                <span class="glyphicon glyphicon-user"></span><br>
+                Find students with similar interests. Build a learning community.</div>
+                <div class="col-md-3">
+                <span class="glyphicon glyphicon-cloud-upload"></span><br>
+                Easily share files, resources and experiences.</div>
+                <div class="col-md-3">
+                <span class="glyphicon glyphicon-pencil"></span><br>
+                Create small groups to study together or collaborate on a project</div>
+                <div class="col-md-3">
+                <span class="glyphicon glyphicon-globe"></span><br>
+                <!-- Learn languages, meet new people, share your knowledge with your chum,  -->Build a network of excellent minds across the globe.</div>
+            </div>
+        </div>
+
+        <script src="assets/js/jquery-2.0.3.min.js"></script>
+        <script src="assets/js/bs.min.js"></script>
+        <script src="assets/js/app.js"></script>
+        <!-- start Dropifi --> <script type='text/javascript' src='https://s3.amazonaws.com/dropifi/js/widget/dropifi_widget.min.js'></script><script type='text/javascript'>document.renderDropifiWidget('70c2f0e75aaee02b1cfef8e927e010c1-1383283917314');</script> <!-- end Dropifi -->
+    </body>
+</html>
