aa68aadc4438abfafd486b01dd9047115e432867
Claude Ayitey <clauditey@gmail.com>
2013-11-14 13:42:01 +0000

    Google analytics code installed on all pages!!!

--- index.php
+++ index.php
@@ -114,6 +114,16 @@
         <script src="assets/js/jquery-2.0.3.min.js"></script>
         <script src="assets/js/bs.min.js"></script>
         <script src="assets/js/app.js"></script>
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-45749635-1', 'studychumapp.appspot.com');
+          ga('send', 'pageview');
+
+        </script>
         <!-- start Dropifi --> <script type='text/javascript' src='https://s3.amazonaws.com/dropifi/js/widget/dropifi_widget.min.js'></script><script type='text/javascript'>document.renderDropifiWidget('70c2f0e75aaee02b1cfef8e927e010c1-1383283917314');</script> <!-- end Dropifi -->
     </body>
 </html>
