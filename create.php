1b77180794a496c6ffab80c715dd3b37c704a6ef
Claude Ayitey <clauditey@gmail.com>
2013-11-15 14:04:07 +0000

    merge changes on the create.php file

--- create.php
+++ create.php
@@ -219,15 +219,10 @@
 
 	<script src="assets/js/jquery-2.0.3.min.js"></script>
 	<script src="assets/js/bs.min.js"></script>
-<<<<<<< HEAD
 	<script src="assets/js/app.min.js"></script>
-=======
-	<script src="assets/js/bs.min.js"></script>
-
 	<script src="assets/js/bootstrap-tagsinput.js"></script>
 	<script src="assets/js/jasny-bootstrap.js"></script>
 
->>>>>>> 6c7ffdd7bc6a71710f3ce3e02aafc2ce792db11d
 	<script>
 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
